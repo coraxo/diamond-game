9edc5005996df0f4e3c266831beb98287ec30458
karl <karl.aws123@gmail.com>
2024-10-18 23:09:47 +0300

    Pull newest image before running app

--- run_prod.sh
+++ run_prod.sh
@@ -3,5 +3,5 @@
 if [ "$1" == "stop" ]; then
   docker-compose -f docker-compose.production.yml down
 else
-  echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin && git pull && docker-compose -f docker-compose.production.yml up -d
+  echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin && git pull && docker pull coraxo/diamond-app:latest && docker-compose -f docker-compose.production.yml up -d
 fi
